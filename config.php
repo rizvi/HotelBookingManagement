fd1106d014b05f0c55ee81f43c326b3f3f0bd74c
rizvi <rizvi3389ict@gmail.com>
2023-11-18 16:37:57 +0600

    Initial Commit

---
+++ config.php
@@ -0,0 +1,20 @@
+<?php
+/*
+This fille contains database configuration assuming you are running mysql using
+user "root" and password ""
+*/
+
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_NAME', 'hotelbookingmanagement');
+define('PORT', '3308');
+
+//Trying to conne ct to database
+$conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME, PORT);
+
+//Check the connection
+if($conn==false){
+    dir('Error: Cannot connect');
+}
+?>
